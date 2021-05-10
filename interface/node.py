2b19984be51c872c3d2a9596128ec2440c5639ca
Frederik Munk Zino <ymzino0002@yahoo.dk>
2021-05-10 21:33:44 +0200

    First commit, first version.

---
+++ interface/node.py
@@ -0,0 +1,247 @@
+"""
+Version: 1.0.1
+Author: Velocity-plus
+Github: https://github.com/Velocity-plus
+Date: 10-05-2021
+For instructions how to use, visit straxrpi.miew.org
+If you want to give me a tip me, check the link above!
+"""
+import json
+import requests
+from requests import exceptions
+from threading import Thread
+from time import sleep
+
+API_URL = "http://localhost:17103"
+API_URL_EXTERNAL = "https://strax.miew.org/api/1.0/stratis/getblockcount"
+
+class TempData:
+    def __init__(self):
+        self.temp_mem = {'password': '',
+                         'passphrase': '',
+                         'walletname': '',
+                         'creationDate': None,
+                         'wallets': [],
+                         'latest_block': -1}
+
+    def get_copy(self):
+        return self.temp_mem
+
+    def clear(self):
+        self.temp_mem['password'] = ''
+        self.temp_mem['passphrase'] = ''
+        self.temp_mem['walletname'] = ''
+        self.temp_mem['creationDate'] = None
+
+
+class Caching(Thread):
+    def __init__(self, data):
+        super().__init__()
+        self.temp_mem = data
+        self.running = True
+        self.pause = 10
+        self.frac = 8
+
+    def run(self):
+        while self.running:
+            self.getLatestBlockCaching()
+        print("Done, bye!")
+
+    def quit(self):
+        self.running = False
+        exit()
+
+    def getLatestBlockCaching(self):
+        subs = API_URL_EXTERNAL
+        try:
+            r = requests.get(subs, timeout=6)
+            if r.status_code == 200:
+                js = json.loads(r.content)
+                self.temp_mem['latest_block'] = js['latest_block']
+        except exceptions.RequestException:
+            pass
+
+        for x in range(self.pause * self.frac):
+            if self.running:
+                sleep(x / self.frac)
+
+
+class NodeAPI:
+    def __init__(self, data):
+        self.API_URL = API_URL
+        self.temp_mem = data
+
+    def is_node_alive(self):
+        subs = "/api/Node/Status"
+        try:
+            r = requests.get(self.API_URL + subs)
+            if r.status_code == 200:
+                return {'succes': True, 'alive': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+            else:
+                return {'succes': True, 'alive': False, 'data': r.content, 'msg': None, 'code': r.status_code}
+        except exceptions.RequestException:
+            return {'succes': False, 'alive': False, 'data': None, 'msg': None}
+
+    def is_fully_synced(self):
+        subs = "/api/Dashboard/Stats"
+        try:
+            r = requests.get(self.API_URL + subs)
+
+            if r.status_code == 200:
+                data = r.text.splitlines()
+
+                fsync = False
+                wallets = []
+                wline = False
+
+                for line in data:
+                    if "Synced with Network" in line:
+                        if "Yes" in line:
+                            fsync = True
+                    if wline:
+                        wallets.append(line.split("/")[0])
+                    if "Wallets" in line:
+                        wline = True
+
+                self.temp_mem['wallets'] = wallets
+                if fsync:
+                    msg = 'Yes'
+                else:
+                    msg = 'No'
+                return {'succes': True, 'synced': fsync, 'data': data, 'msg': msg, 'code': r.status_code}
+            else:
+                return {'succes': False, 'synced': False, 'data': None, 'msg': None, 'code': r.status_code}
+
+        except exceptions.RequestException:
+            return {'succes': False, 'synced': False, 'data': None,
+                    'msg': 'Something went wrong with checking network sync.', 'code': None}
+
+    def is_staking(self):
+        subs = "/api/Staking/getstakinginfo"
+        try:
+            r = requests.get(self.API_URL + subs)
+            js = json.loads(r.content)
+            if r.status_code == 200:
+                sEnabled = js['enabled']
+                sStaking = js['staking']
+                if sEnabled and sStaking:
+                    msg = "Yes"
+                elif sEnabled:
+                    msg = "Enabled (Not staking yet)"
+                else:
+                    msg = "No"
+                return {'succes': True, 'staking': sStaking, 'enabled': sEnabled, 'data': js, 'msg': msg,
+                        'code': r.status_code}
+            else:
+                return {'succes': False, 'staking': False, 'enabled': False, 'data': js,
+                        'msg': 'Not available - please refresh', 'code': r.status_code}
+        except exceptions.RequestException:
+            return {'succes': False, 'staking': False, 'enabled': False, 'data': None,
+                    'msg': 'Something went wrong with checking '
+                           'staking status, try again.', 'code': None}
+
+    def action_recover_wallet(self, mnemoic, passwd, passphrase, walletname, creationDate):
+        subs = "/api/Wallet/create"
+        params = {"mnemonic": mnemoic,
+                  "password": passwd,
+                  "passphrase": passphrase,
+                  "name": walletname,
+                  "creationDate": creationDate}
+        try:
+            r = requests.post(url=self.API_URL + subs, json=params)
+            if r.status_code == 200:
+
+                self.temp_mem['password'] = passwd
+                self.temp_mem['walletname'] = walletname
+                self.temp_mem['creationDate'] = creationDate
+
+                return {'succes': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+            else:
+                return {'succes'
+                        : False, 'data': r.content, 'msg': None, 'code': r.status_code}
+
+        except exceptions.RequestException:
+            return {'succes': False, 'data': None, 'msg': None, 'code': None}
+
+    def action_remove_wallet(self, walletname):
+        subs = "/api/Wallet/remove-wallet"
+        r = requests.delete(url=self.API_URL + subs + "?WalletName=%s" % walletname)
+        if r.status_code == 200:
+            return {'succes': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+        else:
+            return {'succes': False, 'data': r.content, 'msg': None, 'code': r.status_code}
+
+    def get_balance(self, walletname):
+        subs = "/api/Wallet/balance"
+
+        r = requests.get(url=self.API_URL + subs + "?WalletName=%s&IncludeBalanceByAddress=false" % walletname)
+        js = json.loads(r.content)
+        if r.status_code == 200:
+            bList = js["balances"]
+            total = 0
+            for b in bList:
+                total += b["amountConfirmed"] / 100000000
+            return {'succes': True, 'amount': total, 'data': js, 'msg': None, 'code': r.status_code}
+        else:
+            return {'succes': False, 'amount': None, 'data': js, 'msg': None, 'code': r.status_code}
+
+    def get_block_count(self):
+        subs = "/api/BlockStore/getblockcount"
+        r = requests.get(url=self.API_URL + subs)
+        js = json.loads(r.content)
+        if r.status_code == 200:
+            return {'succes': True, 'count': js, 'data': js, 'msg': None, 'code': r.status_code}
+
+        return {'succes': False, 'count': js, 'data': js, 'msg': None, 'code': r.status_code}
+
+    def action_start_staking(self, walletname, password):
+        subs = "/api/Staking/startstaking"
+        params = {"password": password, 'name': walletname}
+        r = requests.post(url=self.API_URL + subs, json=params)
+        if r.status_code == 200:
+            return {'succes': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+        else:
+            return {'succes': False, 'data': r.content, 'msg': "Could not start staking, perhaps wrong password?",
+                    'code': r.status_code}
+
+    def action_stop_staking(self):
+        subs = "/api/Staking/stopstaking"
+        r = requests.post(url=self.API_URL + subs, json="true")
+        if r.status_code == 200:
+            return {'succes': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+        else:
+            return {'succes': False, 'data': r.content, 'msg': None, 'code': r.status_code}
+
+    def action_sync_from_date(self, date, all, walletName):
+        subs = "/api/Wallet/sync-from-date"
+        params = {"date": date, 'all': all, 'walletName': walletName}
+        r = requests.post(url=self.API_URL + subs, json=params)
+        if r.status_code == 200:
+            return {'succes': True, 'data': r.content, 'msg': None, 'code': r.status_code}
+        else:
+            return {'succes': False, 'data': r.content, 'msg': None, 'code': r.status_code}
+
+    def get_sync_info(self):
+        call_gbc = self.get_block_count()
+        cBlock = int(call_gbc['count'])
+        lBlock = int(self.temp_mem['latest_block'])
+        ratio = round((cBlock / lBlock) * 100, 2)
+        if lBlock != -1:
+            if cBlock > lBlock:
+                return "%s/%s (100%%)" % (cBlock, cBlock)
+            return "%s/%s (%s%%)" % (cBlock, lBlock, ratio)
+
+        return "%s (??%%)" % cBlock
+
+    def get_wallets(self):
+        return "".join([w + " " for w in self.temp_mem['wallets']])
+
+    def get_latest_block(self):
+        subs = API_URL_EXTERNAL
+        try:
+            r = requests.get(subs, timeout=6)
+            if r.status_code == 200:
+                js = json.loads(r.content)
+                self.temp_mem['latest_block'] = js['latest_block']
+        except exceptions.RequestException:
+            pass
---
+++ straxcli.py
@@ -0,0 +1,217 @@
+"""
+Version: 1.0.1
+Author: Velocity-plus
+Github: https://github.com/Velocity-plus
+Date: 10-05-2021
+For instructions how to use, visit straxrpi.miew.org
+If you want to give me a tip me, check the link above!
+"""
+
+from time import sleep
+from interface.node import NodeAPI
+from interface.node import Caching
+from interface.node import TempData
+
+Data = TempData()
+Cache = Caching(Data.temp_mem)
+Cache.start()
+Node = NodeAPI(Data.temp_mem)
+
+
+class StraxCLI:
+    def __init__(self, data):
+        Node.get_latest_block()
+        self.temp_data = data
+
+    def input_prompt_pass(self):
+        walletPasswd = input("Password: ")
+        walletPasswdConf = input("Re-type password: ")
+        if walletPasswd != walletPasswdConf:
+            print("Password didn't match, please try again.")
+            self.input_prompt_pass()
+
+        self.temp_data['password'] = walletPasswdConf
+
+    def input_prompt_ph(self):
+        walletPh = input("Passphrase: ")
+        walletPhConf = input("Re-type passphrase: ")
+        if walletPh != walletPhConf:
+            print("Passphrase didn't match, please try again.")
+            self.input_prompt_ph()
+
+        self.temp_data['passphrase'] = walletPh
+
+    def input_temp_info(self):
+        if self.temp_data['walletname'] == '' and self.temp_data['password'] == '':
+            walletName = input("Wallet Name: ")
+            self.temp_data['walletname'] = walletName
+            self.input_prompt_pass()
+
+    def cli_start(self):
+        print(" ")
+        print("> Checking StraxNode connection...")
+        if Node.is_node_alive()['alive']:
+            print("> StraxNode is: Online... Menu initializing.. \n ")
+            sleep(1)
+            self.cli_display()
+        else:
+            print("> StraxNode is offline, please start the StraxNode")
+            print("> CMD: sudo screen dotnet ~/StraxNode/Stratis.StraxD.dll run -mainnet")
+
+    def cli_display(self):
+
+        # Check for staking
+        is_staking = Node.is_staking()
+
+        print("------------- StraxCLI menu -------------")
+        print(">> Staking Status: %s" % is_staking['msg'])
+
+        # Check Sync and Current block
+        is_synced = Node.is_fully_synced()
+        get_sync_info = Node.get_sync_info()
+
+        print(">> Fully Synced: %s | Current Block: %s" % (is_synced['msg'], get_sync_info))
+        print(" ")
+
+        if is_staking['enabled']:
+            print("> 1. Stop Staking")
+        else:
+            print("> 1. Start Staking")
+
+        print("> 2. Check balance")
+        print("> 3. Re-Sync Wallet")
+        print("> 4. Recover Wallet")
+        print("> 5. Remove Wallet")
+        print(" ")
+        print("> 6. Refresh")
+        print("> 7. Quit")
+        print(" ")
+        print("Your Wallets: %s" % (Node.get_wallets()))
+        print("------------------------------------------")
+        print("Please Select an item from the menu")
+        choice = input("Select: ")
+        try:
+            choice = int(choice)
+            self.cli_select(choice)
+        except ValueError:
+            print("< Not a valid choice, please try again")
+            self.cli_start()
+
+    def cli_select(self, choice):
+        if choice == 1:
+            is_staking = Node.is_staking()
+            if is_staking['enabled']:
+                print("> Stopping staking... Please wait")
+                stop_staking = Node.action_stop_staking()
+                if stop_staking['succes']:
+                    print("< Staking stopped sucessfully!")
+            else:
+                self.input_temp_info()
+                print("< Enabling staking for wallet: %s.." % self.temp_data['walletname'])
+                ss = Node.action_start_staking(self.temp_data['walletname'], self.temp_data['password'])
+                if ss['succes']:
+                    print("< Checking if you are actually staking...")
+                    is_synced = Node.is_fully_synced()
+                    if not is_synced['synced']:
+                        print(
+                            "< NOTE: You are not fully synced, staking won't be active before fully synced with the network")
+                        print("< Your sync progression is: %s" % (Node.get_sync_info()))
+                    sleep(8)
+                    is_staking = Node.is_staking()
+                    print("< Stacking: %s" % is_staking['msg'])
+                else:
+                    print("Something went wrong, when trying to stake. Error code: %s" % ss['code'])
+
+            print("< Loading main menu again. Please wait...")
+            sleep(2)
+
+        elif choice == 2:
+            self.question_balance()
+
+        elif choice == 3:
+            self.question_sync_from_date()
+
+        elif choice == 4:
+            self.question_recover_wallet()
+
+        elif choice == 5:
+            self.question_remove_wallet()
+
+        if choice in (1, 2, 3, 4, 5, 6):
+            self.cli_start()
+
+        if choice == 7:
+            print("Bye bye, Thank you (please wait a moment..)")
+            print("Closing script...")
+        else:
+            print("Not a valid number, please use 1-7 (it can take a moment before exiting..)")
+
+        Cache.quit()
+
+    def question_balance(self):
+        print("< Please fill out the fields below:")
+        walletName = input("Wallet Name: ")
+        print("< Retrieving balance for wallet: %s..." % walletName)
+        balance = Node.get_balance(walletName)
+        if balance['succes']:
+            print("> Your balance is %s STRAX (Confirmed)" % balance['amount'])
+        else:
+            print("Something went wrong, please try again. Error code: %s" % balance['code'])
+
+    def question_sync_from_date(self):
+        print("> Please fill out the fields below:")
+        walletName = input("Wallet Name: ")
+        date = input("Date yy-mm-dd: ")
+        print("< Re-syncing you transactions from %s to present." % date )
+        sync_from = Node.action_sync_from_date(date, True, walletName)
+        if sync_from['succes']:
+            print("Syncing started! Nice.")
+        else:
+            print("Something went wrong, please try again. Error code: %s" % sync_from['code'])
+
+
+    def question_recover_wallet(self):
+        print("> Recover Wallet...")
+        print("> Please fill out the fields below")
+        mnemoic = input("Secret Words: ")
+        self.input_prompt_pass()
+        self.input_prompt_ph()
+        walletname = input("Enter any wallet name: ")
+        creationDate = input("Creation date yy-mm-dd: ")
+        print("< Recovering wallet...")
+        recover = Node.action_recover_wallet(mnemoic, self.temp_data['password'], self.temp_data['passphrase'],
+                                             walletname, creationDate)
+        if recover['succes']:
+            print("< Wallet recovered succesfully with alias: %s" % walletname)
+            print("> Syncing your wallet transactions, please wait")
+            sleep(3)
+            sync_tran = Node.action_sync_from_date(creationDate, True, walletname)
+            if sync_tran['succes']:
+                print("< Syncing started! Great.")
+            else:
+                print("< Could not sync your transactions, you will have re-sync in the main menu. Error code: %s" % sync_tran['code'])
+            print("< Loading main menu, please wait...")
+            sleep(5)
+
+        else:
+            print("< Something went wrong, please try recovering your wallet again. Error code: %s" % recover['code'])
+
+    def question_remove_wallet(self):
+        print("<< !!REMOVE WALLET!! >>")
+        walletName = input("Wallet Name: ")
+        confirm = input("> Are you sure want to remove %s (y/n): " % walletName)
+        if confirm.lower() in ("y", "yes"):
+            rm_wallet = Node.action_remove_wallet(walletName)
+            if rm_wallet['succes']:
+                print("< Wallet %s removed succesfully!" % walletName)
+            else:
+                print("< Something went wrong, please try again.. Error code: %s" % rm_wallet['code'])
+        else:
+            print("> Operation cancelled!")
+
+
+print(">>>> Please check https://github.com/Miew-Software/StraxCLI/releases for updates or the guide <<<")
+print(">>>> Current Version: 1.0.1 <<<< ")
+
+StraxCLI = StraxCLI(Data.temp_mem)
+StraxCLI.cli_start()
